f6b1d48a98a80735102bdc217212d83108652f81
Brandon Huffman <bt_huffman@msn.com>
2019-01-05 23:35:53 -0700

    Completed Chapter 3 reading for upcoming camp.

---
+++ chapter-3.php
@@ -0,0 +1,13 @@
+<?php
+		$author = "Steve Ballmer";
+
+		echo "Developers, developers, developers!
+
+		- $author.";
+		$author1 = "Bill Gates";
+
+		$text = " Measuring programming progress by lines of code is like measuring aircraft building progress by weight.
+
+		- $author1.";
+		echo $text;
+?>
